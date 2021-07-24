e4b269f8fc4760c3bc3bccf2eec9de6a88a41bcf
Yusuf Shoair PC <yshoair@gmail.com>
2021-07-25 01:44:30 +0200

    feat: initial commit

---
+++ src/main/java/com/trade_republic/qoutation/InstrumentQoutationApplication.java
@@ -0,0 +1,13 @@
+package com.trade_republic.qoutation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstrumentQoutationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstrumentQoutationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trade_republic/qoutation/InstrumentQoutationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trade_republic.qoutation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstrumentQoutationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
